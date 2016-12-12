0e329bfe96310a387d5811e2b601ba465d15dc89
Marcin Olek <marcin@oke.pl>
2016-12-12 19:39:12 +0100

    lesson4: ready to go

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -277,14 +277,14 @@ public class ForecastFragment extends Fragment {
                 final String UNITS_PARAM = "units";
                 final String DAYS_PARAM = "cnt";
                 final String APPID_PARAM = "APPID";
-
+                String apikey = getString(R.string.OPENWEATHER_API_KEY);
                 Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
                         .appendQueryParameter(QUERY_PARAM, params[0])
                         .appendQueryParameter(FORMAT_PARAM, format)
                         .appendQueryParameter(UNITS_PARAM, units)
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY)
-                        .build();
+                        .appendQueryParameter(APPID_PARAM, apikey)
+                .build();
 
                 URL url = new URL(builtUri.toString());
 
